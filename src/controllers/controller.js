108354a116a2afd1a4ebaebd36efea0c7e10a44a
Renan-Diass <d.renan.rd@gmail.com>
2024-03-27 21:54:22 -0300

    correcao com planilha

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -8,34 +8,64 @@ export default class ControllerExercicio {
     try {
       const num1 = req.body.num1;
       const num2 = req.body.num2;
-
-      const result = servico.Somar(num1, num2)
-
+  
+      // Verificar se num1 e num2 não estão vazios
+      if (!num1 || !num2) {
+        throw new Error("Os valores de num1 e num2 devem ser fornecidos.");
+      }
+  
+      // Validar se num1 e num2 são números
+      if (isNaN(num1) || isNaN(num2)) {
+        throw new Error("Os valores de num1 e num2 devem ser números válidos.");
+      }
+  
+      const result = servico.Somar(parseInt(num1), parseInt(num2));
+  
       res.status(200).json({ result });
     } catch (error) {
-      res.status(500).json({ message: "Erro ao somar" });
+      res.status(500).json({ message: "Erro ao somar", error: error.message });
     }
   }
+  
+  
+  
 
   Subtrair(req, res) {
     try {
       const num1 = req.body.num1;
       const num2 = req.body.num2;
 
-      const result = servico.Subtrair(num1, num2)
+      if (!num1 || !num2) {
+        throw new Error("Os valores de num1 e num2 devem ser fornecidos.");
+      }
+
+      const validaNumero =  /^-?\d*$/;
+      if (!validaNumero.test(num1) || !validaNumero.test(num2)) {
+        throw new Error("Os valores de num1 e num2 devem conter apenas números.");
+      }
+
+      const result = servico.Subtrair(parseInt(num1), parseInt(num2));
 
       res.status(200).json({ result });
     } catch (error) {
-      res.status(500).json({ message: "Erro ao Subtrair" });
+      res.status(500).json({ message: "Erro ao subtrair", error: error.message });
     }
-  }
+  } 
 
   Dividir(req, res) {
     try {
       const num1 = req.body.num1;
       const num2 = req.body.num2;
+      if (!num1 || !num2) {
+        throw new Error("Os valores de num1 e num2 devem ser fornecidos.");
+      }
 
-      const result = servico.Dividir(num1, num2)
+      const validaNumero =  /^-?\d*$/;
+      if (!validaNumero.test(num1) || !validaNumero.test(num2)) {
+        throw new Error("Os valores de num1 e num2 devem conter apenas números.");
+      }
+
+      const result = servico.Subtrair(parseInt(num1), parseInt(num2));
 
       res.status(200).json({ nome: result });
     } catch (error) {
@@ -48,7 +78,16 @@ export default class ControllerExercicio {
       const num1 = req.body.num1;
       const num2 = req.body.num2;
 
-      const result = servico.Multiplicar(num1, num2)
+      if (!num1 || !num2) {
+        throw new Error("Os valores de num1 e num2 devem ser fornecidos.");
+      }
+
+      const validaNumero =  /^-?\d*$/;
+      if (!validaNumero.test(num1) || !validaNumero.test(num2)) {
+        throw new Error("Os valores de num1 e num2 devem conter apenas números.");
+      }
+
+      const result = servico.Subtrair(parseInt(num1), parseInt(num2));
 
       res.status(200).json({ nome: result });
     } catch (error) {
